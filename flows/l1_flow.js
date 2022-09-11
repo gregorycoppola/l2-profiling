5feea45716e2245c483b87bad6ae29bb514c0fac
Greg Coppola <greg@hiro.so>
2022-09-11 16:02:33 -0500

    l1 flow is working!.. just need to start sending L1 transactions, and disable DEBUG logs

--- flows/l1_flow.js
+++ flows/l1_flow.js
@@ -10,6 +10,19 @@ async function sleep(reason, ms) {
   await promise
 }
 
+const color = {};
+color.black ="\x1b[30m";
+color.red = "\x1b[31m";
+color.green = "\x1b[32m";
+color.yellow = "\x1b[33m";
+color.blue = "\x1b[34m";
+color.magenta = "\x1b[35m";
+color.cyan = "\x1b[36m";
+color.white = "\x1b[37m";
+for (var key in color){
+   console.log( color[key] + key);
+}
+
 async function generate_block() {
 
   console.log('generate_block')
@@ -40,11 +53,11 @@ function spawn_bitcoind() {
 
   child.stdout.on('data', data => {
     const trimmed = data.toString().trim()
-    console.log(`${trimmed}`);
+    console.log(color.yellow + `${trimmed}`);
   });
 
   child.stderr.on('data', data => {
-    console.error(`stderr: ${data}`);
+    console.error(`${data}`);
   });
 
   return child
@@ -65,30 +78,32 @@ function spawn_l1() {
 
   child.stderr.on('data', data => {
     const trimmed = data.toString().trim()
-    console.error(`${trimmed}`);
+    console.log(color.cyan + `${trimmed}`);
   });
 
   return child
 }
 
 async function main() {
-  // const child = spawn_bitcoind()
+  const child1 = spawn_bitcoind()
 
-  // console.log('sleeping 1')
-  // await sleep('wait to start', 2500)
-  // console.log('sleeping 2')
+  console.log('sleeping 1')
+  await sleep('wait to start', 2500)
+  console.log('sleeping 2')
 
-  // const NUM_BLOCKS = 10
-  // for (let i = 0; i < NUM_BLOCKS; i++) { 
-  //   console.log("create block", {i})
-  //   generate_block()
+  const child2 = spawn_l1()
 
-  //   const sleepTime = 4000
-  //   console.log(`sleep for ${sleepTime} ms`)
-  //   await sleep('wait to start', sleepTime)
-  // }
 
-  const child = spawn_l1()
+  const NUM_BLOCKS = 10
+  for (let i = 0; i < NUM_BLOCKS; i++) { 
+    console.log("create block", {i})
+    generate_block()
+
+    const sleepTime = 4000
+    console.log(`sleep for ${sleepTime} ms`)
+    await sleep('wait to start', sleepTime)
+  }
+
 }
 
 main()
