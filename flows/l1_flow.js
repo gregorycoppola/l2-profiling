a8fc21f40ca734f84b5b51f6ac7def4fd3d6648d
Greg Coppola <greg@hiro.so>
2022-09-11 16:47:43 -0500

    kind of working but not

--- flows/l1_flow.js
+++ flows/l1_flow.js
@@ -1,6 +1,22 @@
 const { spawn } = require('child_process');
 const axios = require('axios')
 
+const fs = require('fs')
+
+// import {
+//   AnchorMode,
+//   makeContractCall,
+//   ,
+//   broadcastTransaction,
+//   contractPrincipalCV,
+//   standardPrincipalCV,
+//   stringAsciiCV,
+//   uintCV,
+// } from '@stacks/transactions';
+
+// import { StacksTestnet } from '@stacks/network';
+const stacks_network = require('@stacks/network')
+const transactions = require('@stacks/transactions')
 async function sleep(reason, ms) {
   const promise = new Promise((resolve) => {
       // info_log(`start to sleep for: ${reason}`)
@@ -21,6 +37,19 @@ color.cyan = "\x1b[36m";
 color.white = "\x1b[37m";
 for (var key in color){
    console.log( color[key] + key);
+}
+
+async function publishContract(senderKey, contractName, contractFilename, networkUrl, nonce) {
+  const codeBody = fs.readFileSync(contractFilename, { encoding: 'utf-8' });
+  const transaction = await transactions.makeContractDeploy({
+    codeBody, contractName, senderKey, network: new stacks_network.StacksTestnet({url: networkUrl}),
+    anchorMode: transactions.AnchorMode.Any, fee: 50000, nonce
+  });
+  const network = new stacks_network.StacksTestnet({url: networkUrl});
+  const txid = await transactions.broadcastTransaction(
+    transaction, network
+  )
+  return '0x' + txid.txid
 }
 
 async function generate_block() {
@@ -67,7 +96,7 @@ function spawn_l1() {
   const child = spawn('/home/greg/main1/target/release/stacks-node',
     [
       'start',
-      '--config=/home/greg/main1/testnet/stacks-node/conf/mocknet-miner-conf.toml'
+      '--config=/home/greg/main1/testnet/stacks-node/conf/mocknet-miner-conf.toml',
     ]
   );
 
@@ -85,24 +114,44 @@ function spawn_l1() {
 }
 
 async function main() {
+  // bitcoind
   const _child1 = spawn_bitcoind()
 
   console.log('sleeping 1')
   await sleep('wait to start', 2500)
   console.log('sleeping 2')
 
+  // L1
   const _child2 = spawn_l1()
 
-  const NUM_BLOCKS = 10
-  for (let i = 0; i < NUM_BLOCKS; i++) { 
+  // Loop to make the blocks
+  for (let i = 0; i < 2; i++) { 
     console.log("create block", {i})
     generate_block()
 
-    const sleepTime = 4000
+    const sleepTime = 10000
     console.log(`sleep for ${sleepTime} ms`)
     await sleep('wait to start', sleepTime)
   }
 
+  // send the transactions
+  const userKey = '753b7cc01a1a2e86221266a154af739463fce51219d97e4f856cd7200c3bd2a601'
+  const userAddr = 'ST1PQHQKV0RJXZFY1DGX8MNSNYVE3VGZJSRTPGZGM'
+  const L1_URL = "http://localhost:18443"
+  const userPublish0id = await publishContract(userKey, 'trait-standards', '../contracts/trait-standards.clar', L1_URL, 0)
+  const userPublish1id = await publishContract(userKey, 'simple-nft', '../contracts/simple-nft.clar', L1_URL, 1)
+  
+
+    // Loop to make the blocks
+    for (let i = 0; i < 10; i++) { 
+      console.log("create block", {i})
+      generate_block()
+  
+      const sleepTime = 10000
+      console.log(`sleep for ${sleepTime} ms`)
+      await sleep('wait to start', sleepTime)
+    }
+  
 }
 
 main()
