24828e3bfcc67c05f5d60a11d732bfe922dd6618
Greg Coppola <greg@hiro.so>
2022-11-03 13:54:59 -0500

    now output the transactions

--- profiler/mono_server.js
+++ profiler/mono_server.js
@@ -1,26 +1,28 @@
-// const express = require('express')
 import express from 'express';
+import pkg from 'stacks-encoding-native-js';
+const { decodeTransaction } = pkg;
 
-import {
-  info_log,
-} from './logger.js';
-
-
-function makeServer(port) {
+function makeAndLaunchServer(port) {
   const app = express()
   app.use(express.json({limit: '50mb'}));
   app.use(express.urlencoded({extended: true, limit: '50mb'}));
   app.post('*', (req, res) => {
-    // console.log(JSON.stringify(req.query))
-    console.log(JSON.stringify(req.params) + ' ' + JSON.stringify(req.body))
+    var tx_part = ''
+    if (req.body.transactions) {
+      for (const transaction of req.body.transactions) {
+        const decodedTx = decodeTransaction(transaction.raw_tx)
+        tx_part += '|||' + JSON.stringify(decodedTx)
+      }
+    }
+    console.log(JSON.stringify(req.params) + ' ' + JSON.stringify(req.body) + tx_part)
     res.sendStatus(200);
   })
 
   app.listen(port, () => {
-    info_log(`${port} Listener started on port ${port}`)
+    console.log(`${port} Listener started on port ${port}`)
   })
   
   return app
 }
 
-let server = makeServer(50303)
+makeAndLaunchServer(50303)
